20143cabfc4a3147870e80bb9a7abfa62469e77a
anbnyc <alecbarrett@gmail.com>
2017-01-11 22:43:32 -0500

    Revert "Revert "various clean up""
    This reverts commit 8033e65804b9d447991438ca01d61072cbfc018f.

--- src/Legend.js
+++ src/Legend.js
@@ -1,13 +1,15 @@
 import React, { Component } from 'react';
+import _ from 'lodash';
 import './App.css';
 
 function Legend({color}) {
+  var factor = 40;
   return <div>
-    <svg id="legend" height={100}>
+    <svg id="legend" height={4*factor}>
     {color.range.map((d,i) =>
-      <g className="legend" transform={"translate(0,"+i*20+")"}>
-        <rect fill={d} height={16} width={16} />
-        <text x={20} y={12} >{color.domain[i]}</text>
+      <g className="legend" transform={"translate(100,"+i*factor+")"}>
+        <rect fill={d} height={.8*factor} width={.8*factor} />
+        <text x={factor} y={.5*factor} >{_.startCase(color.domain[i])}</text>
       </g>
     )}
     </svg>
